719bf5c3e25405aa2dc04b751447426b5abadc4b
akosENagy <akos.e.nagy@gmail.com>
2017-05-03 14:50:43 +0200

    added write_to_file to write multiple forms at once

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,17 @@
-def write_to_file(form_data_list):
+def add_to_file(form_data_list):
 
     form_data_list = [prop.replace('\r\n', '') for prop in form_data_list]
 
     with open('stories.csv', 'a+') as file:
         file.write(','.join(form_data_list) + "\n")
+
+
+def write_to_file(list_of_forms):
+
+    with open('stories.csv', 'w') as file:
+        for form in list_of_forms:
+            form_data_list = ["{}:{}".format(key, form[key]) for key in form]
+            add_to_file(form_data_list)
 
 
 def get_data_from_file(path):
@@ -18,7 +26,7 @@ def get_data_from_file(path):
             data_dict = {}
             for pair in form_data:
                 data_dict[pair.split(':')[0]] = pair.split(':')[1].strip()
-            data_dict['id'] = i + 1
+            data_dict['id'] = str(i + 1)
             data_final.append(data_dict)
 
         return data_final
