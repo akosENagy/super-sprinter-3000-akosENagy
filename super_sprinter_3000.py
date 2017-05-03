19e3903792da6335a5ab9781f554c4c4b6cf0260
akosENagy <akos.e.nagy@gmail.com>
2017-05-03 14:51:38 +0200

    added delete_story(story_id) and necessary imports

--- super_sprinter_3000.py
+++ super_sprinter_3000.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import data_manager
 
 app = Flask(__name__, static_url_path="/static")
@@ -14,16 +14,16 @@ def render_list():
 
 @app.route('/story')
 def render_form():
-    return render_template('form.html', title="Add New Story")
+    return render_template('form.html', title="Add New Story", user_story={})
 
 
 @app.route('/story', methods=['POST'])
 def save_post_data():
 
     form_data_list = ["{}:{}".format(key, request.form[key]) for key in request.form]
-    data_manager.write_to_file(form_data_list)
+    data_manager.add_to_file(form_data_list)
 
-    return render_template('form.html', title="Add New Story")
+    return render_template('form.html', title="Add New Story", user_story={})
 
 
 @app.route('/story/<story_id>')
@@ -32,6 +32,15 @@ def render_filled_form(story_id):
     user_stories = data_manager.get_data_from_file('stories.csv')
 
     return render_template('form.html', title="Edit Story", user_story=user_stories[int(story_id) - 1])
+
+
+@app.route('/delete/<story_id>')
+def delete_story(story_id):
+    user_stories = data_manager.get_data_from_file('stories.csv')
+    del user_stories[int(story_id) - 1]
+    data_manager.write_to_file(user_stories)
+
+    return redirect(url_for('render_list'))
 
 
 if __name__ == '__main__':
