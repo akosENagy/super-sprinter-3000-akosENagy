0963268179f2b7e531b28842f679244b72e7dac6
akosENagy <akos.e.nagy@gmail.com>
2017-05-03 13:08:54 +0200

    Added static path and variable story rendering

--- super_sprinter_3000.py
+++ super_sprinter_3000.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import data_manager
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 
 @app.route('/')
@@ -14,7 +14,7 @@ def render_list():
 
 @app.route('/story')
 def render_form():
-    return render_template('form.html')
+    return render_template('form.html', title="Add New Story")
 
 
 @app.route('/story', methods=['POST'])
@@ -23,7 +23,15 @@ def save_post_data():
     form_data_list = ["{}:{}".format(key, request.form[key]) for key in request.form]
     data_manager.write_to_file(form_data_list)
 
-    return render_template('form.html')
+    return render_template('form.html', title="Add New Story")
+
+
+@app.route('/story/<story_id>')
+def render_filled_form(story_id):
+
+    user_stories = data_manager.get_data_from_file('stories.csv')
+
+    return render_template('form.html', title="Edit Story", user_story=user_stories[int(story_id) - 1])
 
 
 if __name__ == '__main__':
